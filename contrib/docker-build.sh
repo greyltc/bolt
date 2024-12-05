bc8dbc6f16237b2c06f73c4c9d9382d736b4b1d0
Kate Hsuan <hpa@redhat.com>
2024-12-05 11:35:13 +0800

    contrib: arch: fix build dependency and ignore R0917

--- contrib/docker-build.sh
+++ contrib/docker-build.sh
@@ -36,7 +36,7 @@ if [[ -x "$(command -v lcov)" ]]; then
 fi
 
 if [[ -x $(command -v pylint) ]]; then
-    pylint --disable=C0114,C0115,C0116,C0103,C0209,C0301,C0325,R0022,R0913,R0914,R1714,R1725,R1729,R1732,W0201,W0621,W1514 tests/test-integration
+    pylint --disable=C0114,C0115,C0116,C0103,C0209,C0301,C0325,R0022,R0913,R0914,R0917,R1714,R1725,R1729,R1732,W0201,W0621,W1514 tests/test-integration
 fi
 
 if [[ -x $(command -v codespell) ]]; then
